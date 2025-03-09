d8f8ed0efc1e77ae2e5203cf231bf310c970b1d2
Hiren <hirenwaghmare25@gmail.com>
2025-03-09 19:41:01 +0530

    More pointer problems

--- pointerArray.cpp
+++ pointerArray.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::string characters = "Hiren Waghmare";
-    char *pCharacters = nullptr;
-    float pointerArray[characters.length()];
-    for (int i = 0; i<characters.length(); i++){
-        pCharacters = &characters[i];
-        pointerArray[i] = *pCharacters;
-    }
-    for (int i = 0; i<characters.length();i++){
-        std::cout<< pointerArray[i] << " ";
-    }
+    
     return 0;
 }
